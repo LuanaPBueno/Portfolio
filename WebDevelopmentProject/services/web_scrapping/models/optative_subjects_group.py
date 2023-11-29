e1b7ee55748446a98120809d1c70c449862c99ed
Luana Bueno <140860101+LuanaPBueno@users.noreply.github.com>
2023-11-29 18:13:11 -0300

    Delete WebDevelopmentProject/services directory

--- WebDevelopmentProject/services/web_scrapping/models/optative_subjects_group.py
+++
@@ -1,9 +0,0 @@
-class OptativeSubjectsGroup:
-  code: str
-  name: str
-  subjects: list[str]
-
-  def __init__(self, code: str, name: str, subjects: list[str]) -> None:
-    self.code = code
-    self.name = name
-    self.subjects = subjects
